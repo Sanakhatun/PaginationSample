db1555122e1c7cb2d70495cf4dc57abeb3077ada
Sanakhatun Shaikh <sanakhatun.shaikh@mobicule.com>
2021-07-11 19:57:25 +0530

    Added Interceptors and requested for API.

--- app/src/main/java/com/professional/paginationsample/network/ApiClient.java
+++ app/src/main/java/com/professional/paginationsample/network/ApiClient.java
@@ -2,20 +2,36 @@ package com.professional.paginationsample.network;
 
 import com.professional.paginationsample.repository.remote.api.ApiInterface;
 
+import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
     private static Retrofit retrofit = null;
 
-    static ApiInterface getApiClient() {
+    public static ApiInterface getApiClient() {
+
+        /**
+         * Interceptors are a powerful mechanism that can monitor, rewrite & retry calls
+         */
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        /**
+         * Types of Interceptors:
+         * 1. Application Interceptors -> addInterceptor()
+         * 2. Network Interceptors     -> addNetworkInterceptor()
+         */
+
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
 
         retrofit = new Retrofit.Builder()
                 .baseUrl("https://reqres.in")
                 .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
                 .build();
 
         return retrofit.create(ApiInterface.class);
--- app/src/main/java/com/professional/paginationsample/viewModel/MainActivityViewModel.java
+++ app/src/main/java/com/professional/paginationsample/viewModel/MainActivityViewModel.java
@@ -1,6 +1,33 @@
 package com.professional.paginationsample.viewModel;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
+import com.professional.paginationsample.network.ApiClient;
+import com.professional.paginationsample.repository.model.User;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class MainActivityViewModel extends ViewModel {
+
+    public void fetchUsers() {
+        Call<User> call = ApiClient.getApiClient().fetchUsers();
+
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(Call<User> call, Response<User> response) {
+                if (response != null && response.isSuccessful()) {
+                    Log.i("Response", response.toString());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<User> call, Throwable t) {
+
+            }
+        });
+    }
 }
